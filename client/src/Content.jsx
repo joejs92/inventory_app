ac5cea324afbbc219b1a0e529aea4157aa8001a3
joejs92 <joejs_92@yahoo.com>
2025-03-31 12:15:43 -0400

    start creating content component

--- client/src/Content.jsx
+++ client/src/Content.jsx
@@ -3,3 +3,9 @@ import Button from './button';
 import CategoryButton from './CategoryButton.jsx';
 import CategoryBox from './CategoryBox.jsx';
 import axios from 'axios';
+
+function Content(){
+    const [isDefault, setIsDefault] = useState(true);
+    const [APICategories, setAPICategories] = useState([]);
+    const [listCategories, setListCategories] = useState([]);
+}
